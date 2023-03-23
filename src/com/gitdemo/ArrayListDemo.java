da8255415570d97cfd7cfb09ad78c2293cfdd420
Amardeep <amarundale7@gmail.com>
2023-03-23 21:26:17 +0530

    first commit : added arraylist demo code

---
+++ src/com/gitdemo/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.gitdemo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Amar");
+		list.add("Satywan");
+		list.add("Kalyani");
+		list.add("Mahesh");
+		System.out.println(list);
+	}
+
+}
